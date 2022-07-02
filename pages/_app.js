c1e314fae4d53b1014210c7ee4559aa2e1bf46d3
Kyubiful <ser.zabala@gmail.com>
2022-07-02 17:09:50 +0200

    feat(): added global components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,81 @@
-import '../styles/globals.css'
+import { TopCase } from '../components/Case/TopCase'
+import { BottomCase } from '../components/Case/BottomCase'
+import { ButtonCase } from '../components/Case/ButtonCase'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import styles from '../styles/global.module.css'
+
+const screenVariants = {
+  open: {
+    height: 600,
+    transition: {
+      duration: 1
+    }
+  },
+  closed: {
+    height: 147,
+    transition: {
+      duration: 1
+    }
+  }
+}
+
+const contentScreenVariants = {
+  open: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 1
+    }
+  },
+  closed: {
+    opacity: 0
+  }
+}
+
+function MyApp ({ Component, pageProps, router }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isScreenOpen, setIsScreenOpen] = useState(false)
+  const [buttonIsOpen, setButtonIsOpen] = useState(false)
+
+  const openDex = () => {
+    setIsOpen(!isOpen)
+    setButtonIsOpen(!buttonIsOpen)
+    setIsScreenOpen(!isScreenOpen)
+  }
+
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <div className={styles.container}>
+      <div className={styles.pokedexCase}>
+        <TopCase/>
+        <ButtonCase onClick={openDex} buttonIsOpen={buttonIsOpen} />
+        <motion.div
+          className={styles.pokedexScreen}
+          variants={screenVariants}
+          animate={isOpen ? 'open' : 'closed'}
+        >
+            <motion.div
+              className={styles.pokedexScreenContainer}
+              variants={contentScreenVariants}
+              animate={isScreenOpen ? 'open' : 'closed'}
+            >
+              <AnimatePresence exitBeforeEnter>
+                <Component {...pageProps} key={router.pathname}/>
+              </AnimatePresence>
+            </motion.div>
+        </motion.div>
+        <BottomCase/>
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default MyApp
