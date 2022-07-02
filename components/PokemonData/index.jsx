40aeb303389a9493c8151cb6cb6527426d98144a
Kyubiful <ser.zabala@gmail.com>
2022-07-02 17:15:03 +0200

    feat(): create pokemon data component

---
+++ components/PokemonData/index.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import styles from './styles.module.css'
+
+import { PokemonType } from '../PokemonType/index'
+
+export const PokemonData = ({ pokemon }) => {
+  return (
+    <div className={styles.pokeDataContainer}>
+      <p className={styles.pokeDataName}>{pokemon.name}</p>
+      <div className={styles.pokeData}>
+        <Image height={150} width={150} src={pokemon.sprite} layout="fixed" priority />
+        <div className={styles.pokeDataContent}>
+          <p className={styles.pokeDataTitles}>
+            { pokemon.types.length === 1 ? 'Type' : 'Types'}
+          </p>
+          <div className={styles.pokeTypeContainer}>
+            { pokemon.types.map((type, key) => {
+              return <PokemonType key={key} type={type.type.name}/>
+            })}
+
+          </div>
+          <p className={styles.pokeDataTitles}>Abilities</p>
+          <div className={styles.pokeAbilitiesContainer}>
+            { pokemon.abilities.map((abilitie, key) => {
+              return <p key={key}>- {abilitie.ability.name}</p>
+            })}
+          </div>
+          <p className={styles.pokeDataTitles}>Height / Weight</p>
+          <div>
+            <p>{pokemon.height} m / {pokemon.weight} kg</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
