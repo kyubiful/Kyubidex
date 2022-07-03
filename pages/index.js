58d75974fc2bff66675b7db9009e174631c11645
Kyubiful <ser.zabala@gmail.com>
2022-07-03 20:41:03 +0200

    feat(): implement redux in home page

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,51 @@
 import Head from 'next/head'
-import { PokemonCard } from '../components/PokemonCard/index'
-import { ScreenContainer } from '../components/ScreenContainer/index'
+import PokemonCardLazy from '../components/PokemonCard/PokemonCardLazy'
 import styles from '../styles/home.module.css'
-import { useState } from 'react'
+import { addPokemons, scrollPosition, numPokemons } from '../reducers/homePokemonReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useRef } from 'react'
+import ScreenContainer from '../components/ScreenContainer'
+
+let canLoad = true
 
 const Home = ({ pokemons }) => {
-  const [pokemon, setPokemon] = useState(pokemons)
-  const [numPokemons, setNumPokemons] = useState(40)
-  const [scroll, setScroll] = useState(200)
-  const apiUrl = `http://localhost:3000/api/pokemon/scroll/${numPokemons}`
+  const dispatch = useDispatch()
+
+  const pokemon = useSelector((state) => state.homePokemon.pokemons)
+  const numPokemon = useSelector((state) => state.homePokemon.numPokemons)
+  const scroll = useSelector((state) => state.homePokemon.scrollPosition)
+  const savedScrollPosition = useSelector((state) => state.homePokemon.savedScrollPosition)
+  const container = useRef()
+
+  useEffect(() => {
+    if (pokemon.length === 0) {
+      dispatch(addPokemons(pokemons))
+      dispatch(numPokemons(40))
+    }
+
+    if (savedScrollPosition !== 0) {
+      container.current.scrollTop = savedScrollPosition
+    }
+  }, [])
 
   const getNextPokemons = () => {
+    const apiUrl = `http://localhost:3000/api/pokemon/scroll/${numPokemon}`
     fetch(`${apiUrl}`)
       .then(res => res.json())
       .then(data => {
-        setPokemon([...pokemon, ...data.pokemonList])
-        setNumPokemons(numPokemons + 40)
+        dispatch(addPokemons(data.pokemonList))
+        dispatch(numPokemons(numPokemon + 40))
+        canLoad = true
       })
       .catch(e => {
       })
   }
 
   const handleScroll = (e) => {
-    if (e.target.scrollTop > scroll) {
+    if (e.target.scrollTop > scroll && canLoad === true) {
+      canLoad = false
       getNextPokemons()
-      setScroll(scroll += 570)
+      dispatch(scrollPosition(scroll + 570))
     }
   }
 
@@ -35,10 +56,11 @@ const Home = ({ pokemons }) => {
         <meta name="description" content="Pokedex to learn NextJS" />
       </Head>
       <ScreenContainer>
-        <div className={styles.container} onScroll={handleScroll}>
-        { pokemon.map((pokemon) => {
-          return <PokemonCard key={pokemon.id} id={pokemon.id} name={pokemon.name} spriteUrl={pokemon.sprite}/>
-        }) }
+        <div className={styles.container} onScroll={handleScroll} ref={container}>
+          {
+            pokemon.map((pokemon) => {
+              return <PokemonCardLazy key={pokemon.id} id={pokemon.id} name={pokemon.name} spriteUrl={pokemon.sprite} container={container}/>
+            }) }
         </div>
       </ScreenContainer>
     </>
