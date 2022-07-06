059f0ecb9a8d6b037680e233503d3406de7594a5
Kyubiful <ser.zabala@gmail.com>
2022-07-06 13:07:12 +0200

    feat(): add search pokemon

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,45 @@
 import Head from 'next/head'
-import PokemonCard from '../components/PokemonCard/index'
-import styles from '../styles/home.module.css'
-import { addPokemons, scrollPosition, numPokemons } from '../reducers/homePokemonReducer'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useRef } from 'react'
+import { addPokemons, saveNextInfiniteScrollVisorPosition, saveNextLimitNumberPokemons } from '../redux/states/home'
+import PokemonCard from '../components/PokemonCard/index'
 import { ScreenContainer } from '../components/ScreenContainer/index'
+import { motion } from 'framer-motion'
+import styles from '../styles/home.module.css'
 
 let canLoad = true
+
+const variants = {
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1
+    }
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      duration: 1
+    }
+  }
+}
 
 const Home = ({ pokemons }) => {
   const dispatch = useDispatch()
 
-  const pokemon = useSelector((state) => state.homePokemon.pokemons)
-  const numPokemon = useSelector((state) => state.homePokemon.numPokemons)
-  const scroll = useSelector((state) => state.homePokemon.scrollPosition)
-  const savedScrollPosition = useSelector((state) => state.homePokemon.savedScrollPosition)
+  const pokemon = useSelector((state) => state.home.pokemons)
+  const numPokemon = useSelector((state) => state.home.numPokemons)
+  const scroll = useSelector((state) => state.home.infiniteScrollVisorPosition)
+  const savedScrollPosition = useSelector((state) => state.home.savedScrollPosition)
   const container = useRef()
+  const [isSearchActive, setIsSeatchActive] = useState(false)
+  const [searchedPokemon, setSearchedPokemon] = useState({})
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     if (pokemon.length === 0) {
       dispatch(addPokemons(pokemons))
-      dispatch(numPokemons(40))
+      dispatch(saveNextLimitNumberPokemons(40))
     }
 
     if (savedScrollPosition !== 0) {
@@ -34,7 +53,7 @@ const Home = ({ pokemons }) => {
       .then(res => res.json())
       .then(data => {
         dispatch(addPokemons(data.pokemonList))
-        dispatch(numPokemons(numPokemon + 40))
+        dispatch(saveNextLimitNumberPokemons(numPokemon + 40))
         canLoad = true
       })
       .catch(e => {
@@ -45,8 +64,34 @@ const Home = ({ pokemons }) => {
     if (e.target.scrollTop > scroll && canLoad === true) {
       canLoad = false
       getNextPokemons()
-      dispatch(scrollPosition(scroll + 570))
+      dispatch(saveNextInfiniteScrollVisorPosition(scroll + 570))
     }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const fd = Object.fromEntries(new FormData(e.target))
+    const name = fd.pokemon
+    const apiUrl = `http://localhost:3000/api/pokemon/all/${name}`
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(data => {
+        if (data.pokemon !== undefined) {
+          setIsSeatchActive(true)
+          setSearchedPokemon(data.pokemon)
+          setError(false)
+          e.target.pokemon.value = ''
+        } else {
+          setError(true)
+        }
+      })
+  }
+
+  const resetPokemonClick = (e) => {
+    e.preventDefault()
+    setIsSeatchActive(false)
+    setSearchedPokemon({})
+    setError(false)
   }
 
   return (
@@ -56,11 +101,37 @@ const Home = ({ pokemons }) => {
         <meta name="description" content="Pokedex to learn NextJS" />
       </Head>
       <ScreenContainer>
-        <div className={styles.container} onScroll={handleScroll} ref={container}>
-          {
-            pokemon.map((pokemon) => {
-              return <PokemonCard key={pokemon.id} id={pokemon.id} name={pokemon.name} spriteUrl={pokemon.sprite} container={container}/>
-            }) }
+        <div className={styles.pageContainer} ref={container} onScroll={handleScroll}>
+          <div className={styles.searchPokemonFormContainer}>
+            <form onSubmit={handleSubmit}>
+              <input type="text" name="pokemon" placeholder="Search"/>
+              <input type="submit" value="Search"/>
+              <button onClick={resetPokemonClick}>Reset</button>
+            </form>
+            { error
+              ? <motion.div
+              className={styles.searchPokemonError}
+              initial={variants.hidden}
+              animate={variants.visible}
+              exit={variants.hidden}
+            >
+               Pokemon not found
+            </motion.div>
+              : <div className={styles.searchPokemonError}/>
+            }
+          </div>
+          <div className={styles.container} >
+            { isSearchActive &&
+              searchedPokemon.map((pokemon) => {
+                return <PokemonCard key={pokemon.id} id={pokemon.id} name={pokemon.name} spriteUrl={pokemon.sprite} container={container}/>
+              })
+            }
+            { !isSearchActive &&
+              pokemon.map((pokemon) => {
+                return <PokemonCard key={pokemon.id} id={pokemon.id} name={pokemon.name} spriteUrl={pokemon.sprite} container={container}/>
+              })
+            }
+          </div>
         </div>
       </ScreenContainer>
     </>
